471fce3e200349720f797f93d332ac2ddc93c72d
Antony Varlamov <Varlamov.AM@phystech.edu>
2022-11-06 15:24:03 +0300

    Classwork materials Lesson_01 from 06.11.2022

---
+++ Lesson_01_Basic_of_programing/Simple_calculator.py
@@ -0,0 +1,9 @@
+num1 = input("Enter first number: ")
+symb = input("Enter digit of operation: ")
+num2 = input("Enter second number: ")
+
+result = eval(num1 + symb + num2)
+
+print(num1 + " " + symb + " " + num2 + " =", result)
+
+#result 
